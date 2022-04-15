f92d0ee747f1ad44221839ac5d5c51e051a0391d
Maghawry <mag.hauwry@gmail.com>
2022-04-15 19:17:34 +0200

    Page does not refresh anymore upon pressing enter.

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,25 +1,31 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-  state = { term: '' };
+  state = { term: '' }
 
   onInputChange = (event) => {
-   this.setState({ term: event.target.value})
+   this.setState({ term: event.target.value })
+  }
+
+  onFormSubmit = event => {
+   event.preventDefault()
+
+   // TODO: Make sure we callback from the parent component
   }
 
   render() {
     return (
       <div className="search-bar ui segment">
-        <form className="ui form">
+        <form onSubmit={ this.onFormSubmit } className="ui form">
           <div className="field">
             <label htmlFor="">
               <h3>Video Search</h3>
             </label>
             <input
               type="text"
-              value={this.state.term}
+              value={ this.state.term }
               placeholder="E.g. Explore Alternatives"
-              onChange={this.onInputChange}
+              onChange={ this.onInputChange }
             />
           </div>
         </form>
