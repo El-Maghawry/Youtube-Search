834926318e51579c0c5b494f4afd575fbe3ff3f3
Maghawry <mag.hauwry@gmail.com>
2022-04-15 19:08:03 +0200

    SearchBar component added to App, currently minimal functionality

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
 
 class SearchBar extends React.Component {
-   render(){
-      return <h3> SearchBar </h3>
-   }
+  state = { term: '' };
+
+  onInputChange = (event) => {
+   this.setState({ term: event.target.value})
+  }
+
+  render() {
+    return (
+      <div className="search-bar ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label htmlFor="">
+              <h3>Video Search</h3>
+            </label>
+            <input
+              type="text"
+              value={this.state.term}
+              placeholder="E.g. Explore Alternatives"
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default SearchBar
+export default SearchBar;
