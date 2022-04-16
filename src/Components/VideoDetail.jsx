55342c0860c66a783bcbfb60ec4829fad4d5f072
Maghawry <mag.hauwry@gmail.com>
2022-04-16 13:20:35 +0200

    Video player added, connected to onClick in VideoItem Thumbnails.

--- src/Components/VideoDetail.jsx
+++ src/Components/VideoDetail.jsx
@@ -5,10 +5,17 @@ const VideoDetail = ({ video }) => {
 
   return (
     <div>
+      <div className="ui embed">
+        <iframe
+          src={`https://www.youtube.com/embed/${video.id.videoId}`}
+          frameBorder="1"
+          title="Video Player"
+        ></iframe>
+      </div>
       <div className="ui segment">
-         <div>{video.snippet.channelTitle}</div>
-         <h4 className="header">{video.snippet.title}</h4>
-         <div>{video.snippet.description}</div>
+        <div>{video.snippet.channelTitle}</div>
+        <h4 className="header">{video.snippet.title}</h4>
+        <div>{video.snippet.description}</div>
       </div>
     </div>
   );
